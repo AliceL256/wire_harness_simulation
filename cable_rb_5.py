2d32b169368a9f9fae86ebe882d75b2036a37bd2
Alice Liu <13318895079@163.com>
2025-09-09 17:34:08 +0800

    deformable cable (warp)

--- cable_rb_5.py
+++ cable_rb_5.py
@@ -1,14 +1,6 @@
-# Copyright (c) 2024 NVIDIA CORPORATION.  All rights reserved.
-# NVIDIA CORPORATION and its licensors retain all intellectual property
-# and proprietary rights in and to this software, related documentation
-# and any modifications thereto.  Any use, reproduction, disclosure or
-# distribution of this software and related documentation without an express
-# license agreement from NVIDIA CORPORATION is strictly prohibited.
 
 ###########################################################################
-# Example Surface Follow
-#
-# Simulates a soft grid to follow a surface and optimizes control parameters
+# Deformable cable on a roll ball (warp)
 ###########################################################################
 
 import os
